e17f74accf415605d5b2d3c6c19b0aabaf480d82
pockays <1159419473@qq.com>
2026-08-17 21:01:51 +0800

    修复

--- Packages/com.vrchat.liuris/Runtime/VRCPhysBoneOptimizer.cs
+++ Packages/com.vrchat.liuris/Runtime/VRCPhysBoneOptimizer.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using System.Collections.Generic;
+using nadena.dev.ndmf;
 
 [AddComponentMenu("咩卡布/PhysBone Optimizer")]
 [DefaultExecutionOrder(-32000)]
-public class VRCPhysBoneOptimizer : MonoBehaviour
+public class VRCPhysBoneOptimizer : MonoBehaviour, INDMFEditorOnly
 {
     [Header("动骨优化")]
     [Tooltip("需要迁移PhysBone的源对象列表（可为空），将对每个对象执行\"迁移PhysBone到Root\"，完成后删除源对象")]
